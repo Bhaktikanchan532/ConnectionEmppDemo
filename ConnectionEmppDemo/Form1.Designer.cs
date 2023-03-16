294ec31d6e164f864eead07b097cabf8f97a4994
Acer <Acer@DESKTOP-FSEDNBO>
2023-03-16 03:36:55 -0700

    Add project files.

---
+++ ConnectionEmppDemo/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ConnectionEmppDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmppId = new System.Windows.Forms.Label();
+            this.lblEmppName = new System.Windows.Forms.Label();
+            this.lblEmppLocation = new System.Windows.Forms.Label();
+            this.txtEmployeeId = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnShowAllEmployee = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblEmppId
+            // 
+            this.lblEmppId.AutoSize = true;
+            this.lblEmppId.Location = new System.Drawing.Point(45, 33);
+            this.lblEmppId.Name = "lblEmppId";
+            this.lblEmppId.Size = new System.Drawing.Size(65, 13);
+            this.lblEmppId.TabIndex = 0;
+            this.lblEmppId.Text = "Employee Id";
+            // 
+            // lblEmppName
+            // 
+            this.lblEmppName.AutoSize = true;
+            this.lblEmppName.Location = new System.Drawing.Point(48, 64);
+            this.lblEmppName.Name = "lblEmppName";
+            this.lblEmppName.Size = new System.Drawing.Size(84, 13);
+            this.lblEmppName.TabIndex = 1;
+            this.lblEmppName.Text = "Employee Name";
+            // 
+            // lblEmppLocation
+            // 
+            this.lblEmppLocation.AutoSize = true;
+            this.lblEmppLocation.Location = new System.Drawing.Point(48, 104);
+            this.lblEmppLocation.Name = "lblEmppLocation";
+            this.lblEmppLocation.Size = new System.Drawing.Size(48, 13);
+            this.lblEmppLocation.TabIndex = 2;
+            this.lblEmppLocation.Text = "Location";
+            // 
+            // txtEmployeeId
+            // 
+            this.txtEmployeeId.Location = new System.Drawing.Point(193, 26);
+            this.txtEmployeeId.Name = "txtEmployeeId";
+            this.txtEmployeeId.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeId.TabIndex = 3;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(193, 61);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeName.TabIndex = 4;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(193, 104);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(100, 20);
+            this.txtLocation.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(35, 165);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(202, 165);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(344, 33);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(344, 71);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(377, 126);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(398, 162);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // btnShowAllEmployee
+            // 
+            this.btnShowAllEmployee.Location = new System.Drawing.Point(493, 307);
+            this.btnShowAllEmployee.Name = "btnShowAllEmployee";
+            this.btnShowAllEmployee.Size = new System.Drawing.Size(142, 23);
+            this.btnShowAllEmployee.TabIndex = 11;
+            this.btnShowAllEmployee.Text = "Show All Employee";
+            this.btnShowAllEmployee.UseVisualStyleBackColor = true;
+            this.btnShowAllEmployee.Click += new System.EventHandler(this.btnShowAllEmployee_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShowAllEmployee);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.txtEmployeeId);
+            this.Controls.Add(this.lblEmppLocation);
+            this.Controls.Add(this.lblEmppName);
+            this.Controls.Add(this.lblEmppId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmppId;
+        private System.Windows.Forms.Label lblEmppName;
+        private System.Windows.Forms.Label lblEmppLocation;
+        private System.Windows.Forms.TextBox txtEmployeeId;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnShowAllEmployee;
+    }
+}
+
